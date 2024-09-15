6c66ad11405fa47e3eadd535831286284ee3c6a5
Your Name <rzhyl05@gmail.com>
2024-09-15 17:34:28 +0800

    your commit message

---
+++ strings.js
@@ -0,0 +1,19 @@
+let favActorFirstName = "Tom ";
+console.log("First Name: ", favActorFirstName);
+
+let favActorLastName = "Holland";
+console.log("Last Name: ", favActorLastName);
+
+let fullName = favActorFirstName + favActorLastName;
+console.log("Full Name: ", fullName);
+
+let uppercase = fullName.toLowerCase();
+console.log("Lowercase Full Name: ", uppercase);
+
+let petName = "molly";
+let lowercase = petName.toUpperCase();
+console.log("Capitalized Pet Name: ", lowercase);
+
+let message = "My favorite actor is " + fullName.toUpperCase();
+message += " and his best show is Silicon Valley";
+console.log("Message: ", message);
